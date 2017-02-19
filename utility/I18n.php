fde839e0079fdabcd393b2c73200d48b551c0c2e
kdays <admin@kdays.cn>
2017-02-19 15:21:52 +0800

    fix I18n

--- utility/I18n.php
+++ utility/I18n.php
@@ -8,93 +8,29 @@
 
 namespace Akari\utility;
 
-use Akari\config\ConfigItem;
-use Akari\Context;
-use Akari\system\exception\AkariException;
+use Akari\system\ioc\DIHelper;
 
 Class I18n {
+    
+    use DIHelper;
 
-    protected static $_data = [];
-    protected static $loadedPackages = [];
-
-    protected static function getPath($name, $useRelative) {
-        $languageId = Context::env(ConfigItem::LANGUAGE_ID, NULL, FALSE);
-        $suffix = ".php";
-
-        $paths = [];
-        
-        if (!$useRelative) {
-            $baseDir = dirname($name). DIRECTORY_SEPARATOR;
-            $name = basename($name);
-        } else {
-            $baseDir = implode(DIRECTORY_SEPARATOR, [Context::$appEntryPath, 'language', '']);
-        }
-
-        if ($languageId) {
-            $paths[] = $baseDir. $languageId. DIRECTORY_SEPARATOR. $name. $suffix;
-            $paths[] = $baseDir. $languageId. ".". $name. $suffix;
-        }
-
-        $paths[] = $baseDir. $name. $suffix;
-        
-        foreach ($paths as $path) {
-            if (file_exists($path)) return $path;
-        }
-        
-        return FALSE;
+    /**
+     * @return \Akari\system\i18n\I18n
+     */
+    protected static function getHandler() {
+        return self::_getDI()->getShared('lang');
     }
 
     public static function loadPackage($packageId, $prefix = "", $useRelative = TRUE) {
-        if (isset(self::$loadedPackages[ $prefix. $packageId ])) {
-            return FALSE;
-        }
-        
-        $path = self::getPath($packageId, $useRelative);
-        if (!$path) {
-            throw new LanguageNotFound($packageId);
-        }
-
-        self::$loadedPackages[ $prefix. $packageId ] = true;
-
-        $now = require($path);
-        foreach ($now as $key => $value) {
-            self::$_data[ $prefix. $key ] = $value;
-        }
+        return self::getHandler()->loadPackage($packageId, $prefix, $useRelative);
     }
     
     public static function has($id, $prefix = "") {
-        $id = $prefix. $id;
-        return array_key_exists($id, self::$_data);
+        return self::getHandler()->has($id, $prefix);
     }
 
     public static function get($id, $L = [], $prefix = "") {
-        if (!self::has($id, $prefix)) {
-            return $id;
-        }
-        
-        $lang = self::$_data[ $prefix. $id ];
-        foreach($L as $key => $value){
-            $lang = str_replace("%$key%", $value, $lang);
-        }
-
-        // 处理![语言句子] 或被替换成L(语言句子)
-        $lang = preg_replace_callback('/\!\[(\S+)\]/i', function($matches){
-            if (isset($matches[1])) {
-                return I18n::get($matches[1]);
-            }
-
-            return $matches[0];
-        }, $lang);
-
-        return $lang;
-    }
-
-}
-
-Class LanguageNotFound extends AkariException  {
-
-    public function __construct($packageId) {
-        $this->message = "Language Package [ ". $packageId. " ] not found";
+        return self::getHandler()->get($id, $L, $prefix);
     }
 
 }
